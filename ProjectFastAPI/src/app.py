1ee405882dad59463cb08f224e0efa81c3885110
DmitryMogilnikov <d.mogqs@gmail.com>
2023-02-27 16:54:00 +0300

    refactoring

--- ProjectFastAPI/src/app.py
+++ ProjectFastAPI/src/app.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
 from src.api.base_router import router
-from src.db.db import Session
 
 app = FastAPI(
     title='FastAPI приложение',
@@ -10,5 +9,3 @@ app = FastAPI(
 )
 
 app.include_router(router)
-
-session = Session()
